dd07ff6051df9cac2b6c20718bcc341232c527bf
JonasKirchner02 <kirchnerj.21@gmail.com>
2025-09-04 18:38:47 +0200

    control

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:8080");
 
+let controlTD = document.querySelector('.controllTD')
+
+
+
+controllTD.addEventListener('input', (event) => {
+console.log(controllTD.value)
+});
+
+
 // Connection opened
 socket.addEventListener("open", (event) => {
   socket.send("Hello Server!");
